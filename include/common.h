17d6a860653d7276f741ef79132068d815ed6a3a
zverorx <zveror1806@gmail.com>
2025-10-12 15:06:26 +0300

    docs: add copyright documentation to each source file and LICENSE

--- include/common.h
+++ include/common.h
@@ -1,3 +1,23 @@
+/* 
+ * This file is part of jsonfs.
+ * jsonfs - File system for working with JSON.
+ *
+ * Copyright (C) 2025 Egorov Konstantin
+ *
+ * jsonfs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * jsonfs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jsonfs. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file
  * @brief Common types, macros, and private data for JSONFS.
