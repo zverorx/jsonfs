af8bedd3a3277bb29e9d0209deb712190c0f7a84
zverorx <zveror1806@gmail.com>
2025-10-26 01:08:39 +0300

    test: add a more secure test, and key support

--- test/test.sh
+++ test/test.sh
@@ -7,23 +7,31 @@ set -e
 
 test_dir="$(cd $(dirname $BASH_SOURCE[0]) && pwd)"
 exec_file="$test_dir/../bin/jsonfs"
-json_file="$1"
+json_file="$PWD/$1"
 mount_point="$test_dir/mnt"
 
-if [ -z "$json_file" ]
-then
-	json_file="$test_dir/ex_obj.json"
+red_is_enabled=false
+green_is_enabled=false
+blue_is_enabled=false
+
+options="$2"
+if [ -z "$options" ] ; then
+	red_is_enabled=true
+	green_is_enabled=true
+	blue_is_enabled=true
 fi
 
-if [ ! -f "$exec_file" ] 
-then
+if echo "$options" | egrep -q r ; then red_is_enabled=true ; fi
+if echo "$options" | egrep -q g ; then green_is_enabled=true ; fi
+if echo "$options" | egrep -q b ; then blue_is_enabled=true ; fi
+
+if [ ! -f "$exec_file" ] ; then
 	echo "Not found $exec_file" >&2
 	exit 1
 fi
 
-if [ ! -f "$json_file" ]
-then
-	echo "Not found $json_file" >&2
+if [ ! -f "$json_file" ] ; then
+	echo "Error: JSON file not found" >&2
 	exit 1
 fi
 
@@ -33,39 +41,40 @@ if ! "$exec_file" "$json_file" "$mount_point" ; then
 	rmdir "$mount_point"
 	echo "Failed to mount $exec_file" >&2
 	exit 1
+	else cd "$mount_point"
 fi
 
+trap 'cd $test_dir ; sync ; fusermount3 -u $mount_point ; rmdir $mount_point' ERR EXIT
 
 ########## Test start ##########
+if [ "$red_is_enabled" == true ] ; then
+	echo -e "\e[31m**************************************************\e[0m" # red
+	echo "$json_file:"
+	cat "$json_file"
+	echo -e "\e[31m**************************************************\e[0m" # red
+	echo 
+fi
 
-echo -e "\e[31m**************************************************\e[0m"
-echo "$json_file:"
-cat "$json_file"
-echo -e "\e[31m**************************************************\e[0m"
-cd "$mount_point"
-echo 
-echo -e "\e[32m**************************************************\e[0m"
-ls -Rla
-echo -e "\e[32m**************************************************\e[0m"
-echo
-echo -e "\e[34m**************************************************\e[0m"
+if [ "$green_is_enabled" == true ] ; then
+	echo -e "\e[32m**************************************************\e[0m" # green
+	ls -Rla
+	echo -e "\e[32m**************************************************\e[0m" # green
+	echo
+fi
 
-for file in $(find)
-do
-	if [ -f "$file" ] ; then
-		echo -n "$file: "
-		cat "$file"
-		echo
-	fi
-done
-echo -e "\e[34m**************************************************\e[0m"
+if [ "$blue_is_enabled" == true ] ; then
+	echo -e "\e[34m**************************************************\e[0m" # blue
 
+	for file in $(find)
+	do
+		if [ -f "$file" ] ; then
+			echo -n "$file: "
+			cat "$file"
+			echo
+		fi
+	done
+	echo -e "\e[34m**************************************************\e[0m" #blue
+fi
 ########## Test end ##########
-
-
-########## Unmounting ##########
-cd ..
-fusermount3 -u "$mount_point"
-rmdir "$mount_point"
 
 exit 0
